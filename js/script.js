9b223188978e2ca40a6c2428ad4face9532879a3
Aleksandra Joachimiak <joachim07@poczta.onet.pl>
2020-06-22 14:53:39 +0200

    Use var: computerMove and var: playerMove

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
-let computerMove = `kamień`;
+let computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + ' Jeśli Twój ruch to papier to wygrywasz...');
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('OK, czekam teraz na Twój ruch...');
+printMessage('-----------------------------------');
+
+let playerMove = 'papier';
+printMessage('Zagrałe(a)ś ' + playerMove + ' Czyli mnie pokonałe(a)ś');
