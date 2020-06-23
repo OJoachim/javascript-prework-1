11a559397d32963e4d06ac2fd04d98aeb3355070
Aleksandra Joachimiak <joachim07@poczta.onet.pl>
2020-06-23 23:33:11 +0200

    Modify script.js to more orderly, readable

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
-function playGame(playerInput){
+function playGame(playerInput) {
+  
+  /* FUNKCJE POMOCNICZNE */
+  function displayResult(argComputerMove, argPlayerMove) {
+    printMessage('WYNIK POTYCZKI (ja - Ty): ' + argComputerMove + ' - ' + argPlayerMove);
 
-  function clearMessages() {}
-
-  // gracz wybiera ruch zanim wybierze komputer
-  console.log('Gracz wpisał: ' + playerInput);
-
-  // komputer losowo wybiera ruch
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-  console.log('Wylosowana liczba to: ' + randomNumber);
-
-  let playerMove = getMoveName(playerInput);
-  let computerMove = getMoveName(randomNumber);
-
+	 if ((argComputerMove == 'kamień' && argPlayerMove == 'papier') 
+      || (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') 
+      || (argComputerMove == 'papier' && argPlayerMove == 'nożyce')) {
+      printMessage('Gratuluję! Wygrywasz!');
+	 } else if (argComputerMove === argComputerMove) {
+     	printMessage('Fajnie. Jest remis! Wybraliśmy to samo');
+	 } else {
+     printMessage('Hura! Wygrałem! Ty tym razem przegrywasz :(');
+	 }
+  }
+  
   function getMoveName(argMoveId){
     if(argMoveId == 1){
       return 'kamień';
@@ -20,52 +22,33 @@ function playGame(playerInput){
       return 'papier';
     } else if(argMoveId == 3){
       return 'nożyce';
-    } else {
-      printMessage('Nie ma ruchu oznaczonego jako: ' + argMoveId + '. Możesz wybrać: 1, 2 lub 3');
-    return 'niewłaściwe zagranie';
     }
   }
 
-  console.log('moves:', playerMove, computerMove);
-  console.log('Ty zagrałe(a)ś: ' + playerMove);
-  console.log('computer zagrał: ' + computerMove);
+  /* PROCES GRY */
+  clearMessages();
+  // komputer losowo wybiera swój ruch
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  let playerMove = getMoveName(playerInput);
+  let computerMove = getMoveName(randomNumber);
 
-  // Wynik gry
-
-  let result = displayResult(computerMove, playerMove);
-
-  function displayResult(argComputerMove, argPlayerMove) {
-	 console.log('argumenty: ' + argComputerMove + ', ' + argPlayerMove);
-    printMessage('WYNIK POTYCZKI (ja - Ty): ' + argComputerMove + ' - ' + argPlayerMove);
-
-	 if(( argComputerMove == 'kamień' && argPlayerMove == 'papier') || (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') || (argComputerMove == 'papier' && argPlayerMove == 'nożyce')) {
-    printMessage('Gratuluję! Wygrywasz!');
-	 } else if ((argComputerMove == 'papier' && argPlayerMove == 'kamień') || (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') || (argComputerMove == 'nożyce' && argPlayerMove == 'papier')){
-    printMessage('Hura! Wygrałem! Ty tym razem przegrywasz :(');
-	 } else if (playerMove == 'niewłaściwe zagranie' && (argComputerMove == 'kamień' || argComputerMove == 'papier' || argComputerMove == 'nożyce')){
-	   printMessage('Wygrywam walkowerem, bo zastosowałe(a)ś niewłaściwe zagranie. Masz wybrać cyfrę: 1, 2 lub 3!');
-	 } else if (argComputerMove == 'niewłaściwe zagranie' && (argPlayerMove == 'kamień' || argPlayerMove == 'papier' || argPlayerMove == 'nożyce')){
-	   printMessage('Przegrywam, bo zastosowałem niewłaściwe zagranie');
-	 } else {
-	   printMessage('Fajnie. Jest remis! Wybraliśmy to samo');
-	 }
-  }
-
+  // Ustalamy kto wygrał
+  displayResult(computerMove, playerMove);
+  
+  // Dopisz linię
+  printMessage('-------------------------------------------------------');
 }
 
 // Uruchamianie guzików
 
 document.getElementById('play-rock').addEventListener('click', function(){
   playGame(1);
-  printMessage('-------------------------------------------------------');
 });
 
 document.getElementById('play-paper').addEventListener('click', function(){
   playGame(2)
-  printMessage('-------------------------------------------------------');;
 });
 
 document.getElementById('play-scissors').addEventListener('click', function(){
   playGame(3);
-  printMessage('-------------------------------------------------------');
 });
